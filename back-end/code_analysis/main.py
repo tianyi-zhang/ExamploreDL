f814c0ef6aa9cac6e7e33ce7f3d880dc3d04535e
YanLitao <yanlitao@outlook.com>
2020-03-14 22:57:17 -0400

    Modify the code analysis to make it can generate a json with same structure with example.json.

---
+++ back-end/code_analysis/main.py
@@ -0,0 +1,14 @@
+from test_json import traverse_dic
+import json
+if __name__ == "__main__":
+
+	dic = '/Users/mac/Desktop/R/backen/code_analysis/test_folder'
+	output_dic = {"Path": dic, "Tasks": ['cv'], "Dataset": [], "Models": {}}
+	traveres_ob = traverse_dic(dic)
+	model_dic = traveres_ob.traverse_files()
+	print(model_dic)
+	json_object = json.dumps(model_dic, indent=4)
+
+	with open("model.json", "w") as outfile:
+		outfile.write(json_object)
+		outfile.close()
